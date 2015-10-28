f8f239d0f4a699bf995a0c5ec35d2740e40e35f8
Martin Frey <martin.frey@texelia.com>
2015-10-28 15:08:52 +0100

    add JMH profile (benchmark) and a cache/nocache benchmark

---
+++ src/jmh/java/ch/mfrey/jackson/antpathfilter/test/FilterBenchmark.java
@@ -0,0 +1,81 @@
+package ch.mfrey.jackson.antpathfilter.test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ch.mfrey.jackson.antpathfilter.Jackson2Helper;
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(3)
+@Threads(2)
+public class FilterBenchmark {
+
+    private final Jackson2Helper jackson2Helper = new Jackson2Helper();
+    private final List<User> users = new ArrayList<User>();
+
+    private final Map<Integer, ObjectMapper> cache = new ConcurrentHashMap<Integer, ObjectMapper>();
+
+    
+    /**
+     * Convenience method to simply write an object to a json representation using the given filters.
+     * 
+     * @param value
+     *            Any object that can be serialized to json
+     * @param filters
+     *            The desired filters to be used
+     * @return The json representation
+     */
+    public String writeCachedFiltered(final Object value, final String... filters) {
+        try {
+            int key = Arrays.hashCode(filters);
+            if (!cache.containsKey(key)) {
+                cache.put(key, jackson2Helper.buildObjectMapper(filters));
+            }
+            return cache.get(key).writeValueAsString(value);
+        } catch (IOException ioe) {
+            throw new RuntimeException("Could not write object filtered.", ioe);
+        }
+    }
+    
+    public FilterBenchmark() {
+        for (int i = 0; i < 1000; i++) {
+            users.add(User.buildMySelf());
+        }
+    }
+
+    @Benchmark
+    public void measureCached() {
+        for (int i = 0; i < 100; i++) {
+            writeCachedFiltered(users, "*", i % 2 == 0 ? "-manager" : "-reports", i % 3 == 0 ? "-address" : "-reports");
+        }
+    }
+
+    @Benchmark
+    public void measureNotCached() {
+        for (int i = 0; i < 100; i++) {
+            jackson2Helper.writeFiltered(users, "*", i % 2 == 0 ? "-manager" : "-reports", i % 3 == 0 ? "-address" : "-reports");
+        }
+    }
+}
--- src/test/java/ch/mfrey/jackson/antpathfilter/test/ConcurrencyTest.java
+++ src/test/java/ch/mfrey/jackson/antpathfilter/test/ConcurrencyTest.java
@@ -1,5 +1,6 @@
 package ch.mfrey.jackson.antpathfilter.test;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -8,43 +9,55 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
 import ch.mfrey.jackson.antpathfilter.Jackson2Helper;
 
 public class ConcurrencyTest {
 
+    private final class UserRunner implements Callable<Object> {
+        private final AtomicInteger count;
+        private final User user;
+
+        private UserRunner(User user, AtomicInteger count) {
+            this.user = user;
+            this.count = count;
+        }
+
+        public Object call() throws Exception {
+            try {
+                for (int i = 0; i < 30; i++) {
+                    String json = jackson2Helper.writeFiltered(user, "*", i % 2 == 0 ? "-manager" : "-reports", i % 3 == 0 ? "-address" : "-reports");
+                    if (i % 2 == 0 && i % 3 == 0) {
+                        Assert.assertEquals("{\"email\":\"somewhere@no.where\",\"firstName\":\"Martin\",\"lastName\":\"Frey\",\"reports\":[{},{},{},{},{},{},{},{},{},{}]}", json);
+                    } else if (i % 2 == 0) {
+                        Assert.assertEquals("{\"address\":{},\"email\":\"somewhere@no.where\",\"firstName\":\"Martin\",\"lastName\":\"Frey\"}", json);
+                    } else if (i % 3 == 0) {
+                        Assert.assertEquals("{\"email\":\"somewhere@no.where\",\"firstName\":\"Martin\",\"lastName\":\"Frey\",\"manager\":{}}", json);
+                    } else {
+                        Assert.assertEquals("{\"address\":{},\"email\":\"somewhere@no.where\",\"firstName\":\"Martin\",\"lastName\":\"Frey\",\"manager\":{}}", json);
+                    }
+                }
+                count.incrementAndGet();
+            } catch (Throwable t) {
+                System.out.println(t.getMessage());
+            }
+            return null;
+        }
+    }
+
     private final Jackson2Helper jackson2Helper = new Jackson2Helper();
 
     @Test
-    public void testExclusion() throws JsonProcessingException, InterruptedException {
+    public void testConcurrency() throws InterruptedException {
         final AtomicInteger count = new AtomicInteger(0);
+        User user = User.buildMySelf();
+        UserRunner runner = new UserRunner(user, count);
+
         ExecutorService fixedThreadPool = Executors.newFixedThreadPool(10);
         for (int i = 0; i < 1000; i++) {
-            fixedThreadPool.submit(new Runnable() {
-                public void run() {
-                    User user = User.buildMySelf();
-                    long id = Thread.currentThread().getId();
-                    String json = jackson2Helper.writeFiltered(user, "*", id % 2 == 0 ? "-manager" : "-reports", id % 3 == 0 ? "-address" : "-reports");
-                    try {
-                        if (id % 2 == 0 && id % 3 == 0) {
-                            Assert.assertEquals("{\"email\":\"somewhere@no.where\",\"firstName\":\"Martin\",\"lastName\":\"Frey\",\"reports\":[{},{},{},{},{},{},{},{},{},{}]}", json);
-                        } else if (id % 2 == 0) {
-                            Assert.assertEquals("{\"address\":{},\"email\":\"somewhere@no.where\",\"firstName\":\"Martin\",\"lastName\":\"Frey\"}", json);
-                        } else if (id % 3 == 0) {
-                            Assert.assertEquals("{\"email\":\"somewhere@no.where\",\"firstName\":\"Martin\",\"lastName\":\"Frey\",\"manager\":{}}", json);
-                        } else {
-                            Assert.assertEquals("{\"address\":{},\"email\":\"somewhere@no.where\",\"firstName\":\"Martin\",\"lastName\":\"Frey\",\"manager\":{}}", json);
-                        }
-                        count.incrementAndGet();
-                    } catch (Throwable t) {
-                        System.out.println(t.getMessage());
-                    }
-                }
-            });
+            fixedThreadPool.submit(runner);
         }
         fixedThreadPool.shutdown();
-        fixedThreadPool.awaitTermination(1, TimeUnit.MINUTES);
+        fixedThreadPool.awaitTermination(5, TimeUnit.MINUTES);
         Assert.assertEquals(1000, count.intValue());
     }
 
